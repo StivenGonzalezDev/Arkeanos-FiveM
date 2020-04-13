112e15693d545d1cb39e83362e344fb605372676
StivenDev <59249760+StivenGonzalezDev@users.noreply.github.com>
2020-04-13 01:03:40 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,17 +1,12 @@
---[[ SESX CHANGE START ]]--
--- THIS HAS BEEN ADDED TO HIDE THE BRIDGE DURING LOADIN AND ALLOW THE LOADING SCREEN TO DO IT'S JOB.
--- Thanks to ChristopherM for how-to: https://forum.fivem.net/t/how-to-remove-the-bridge-using-a-new-or-existing-loading-screen-resource/798407
-    -- Variable to check if native has already been run
 local Ran = false
 
--- Wait until client is loaded into the map
+-- Espera a spawnear personaje para borrar pantalla de carga
 AddEventHandler("playerSpawned", function ()
-    -- If not already ran
+
     if not Ran then
-        -- Close loading screen resource
+        -- Cierra la pantalla de carga
         ShutdownLoadingScreenNui()
-        -- Set as ran
+        -- Set del ran
         Ran = true
     end
 end)
---[[ SESX CHANGE END ]]--
